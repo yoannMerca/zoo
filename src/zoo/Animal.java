80c82262003f526d51756ace562ec4c6f2119d9e
yoann <you@example.com>
2018-11-20 12:39:11 +0100

    tp1

---
+++ src/zoo/Animal.java
@@ -0,0 +1,15 @@
+package zoo;
+
+public class Animal {
+		protected String nom;
+		protected String type;
+		
+		
+		public Animal(String nom, String type) {
+			super();
+			this.nom = nom;
+			this.type = type;
+		}
+	
+	
+}
